bcaf10cc69105927ba3d80dc0244295e6c5225c2
DanieleFic <reb_ficus22@hotmail.it>
2022-02-02 16:16:07 +0100

    creato axios

---
+++ src/components/Macro/Main.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="ms_lista">
+        <Cerca
+        @cerca="filtraFilm"/>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+import Cerca from '../commons/cerca.vue'
+export default {
+    name: 'Main',
+    props: {
+    
+    },
+    components: {
+        Cerca
+    },
+    data(){
+        return{
+            apiUrl: 'https://api.themoviedb.org/3/search/movie',
+            movieArray: [],
+        }
+    },
+    created() {
+        this.getFilm();
+    },
+    methods: {
+        getFilm(){
+            axios.get(this.apiUrl, {
+            params: {
+            api_key:"be63dc6417cc951f2c485376b9ceedd3",
+            query:"zorro" 
+            }
+        })
+        .then( (payload) => {
+            this.movieArray = payload.data;
+            console.log(this.movieArray)
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });  
+        }
+    },
+        
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+</style>
---
+++ src/components/commons/cerca.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input 
+        type="text"
+        v-model.trim="inputText"
+        @keyup="$emit('cercafilm',inputText)">
+
+        <input
+        type="button"
+        value="Cerca"
+        @click.prevent="$emit('filtra',inputText)">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"cerca"
+}
+</script>
+
+<style>
+
+</style>
